fd8582c5f361d199b754de5ee34155ed297d8803
infsolution <clinfsolution@gmail.com>
2021-07-13 16:41:16 -0300

    updating modal and product register

--- src/Components/Menu/AddProduct.js
+++ src/Components/Menu/AddProduct.js
@@ -29,6 +29,11 @@ const AddProduct =()=>{
     const [category, setCategory] = React.useState('')
     const [printers, setPrinters] = React.useState({id:'', name:''})
     const [categories, setCategories] = React.useState({id:'', name:''})
+    const [aproduct, setAproduct] = React.useState(true)
+    const [acomplement, setAcomplement] = React.useState(false)
+    const [aimage, setAimage] = React.useState(false)
+    const [product,setProduc] = React.useState(null)
+    const[loadind, setLoading] = React.useState(false)
 
 
     const setData=(data)=>{
@@ -61,7 +66,9 @@ const AddProduct =()=>{
     const handleSubimit = async  event=>{
         event.preventDefault()
        try {
-           const body = {
+           setLoading(true)
+           if(!product){
+            const body = {
                 name: event.target.name.value,
                 description:event.target.description.value,
                 code:event.target.code.value,
@@ -75,85 +82,121 @@ const AddProduct =()=>{
            const {url, options} = POST_PRODUCT(token, body)
            const response = await fetch(url, options)
            if(!response.ok) throw new Error(`Error: ${response.statusText}`)
+           const {product} = await response.json()
+           setProduc(product)
+           showAcomplement()
+           }else{
+               showAcomplement()
+           }
            
-           window.location.href = "/cardapio";
+           //window.location.href = "/cardapio";
        } catch (error) {
            console.log(error)
        }
+       finally{
+        setLoading(false)
+       }
     } 
+    const showAproduct =()=>{
+        setAcomplement(false)
+        setAimage(false)
+        setAproduct(true)
+    }
+
+    const showAcomplement =()=>{
+        setAproduct(false)
+        setAimage(false)
+        setAcomplement(true)
+    }
+    const showAimage =()=>{
+        setAcomplement(false)
+        setAproduct(false)
+        setAimage(true)
+    }
 
     React.useEffect(()=>{
         getData()
     },[])
 
     return(
-        <>
-        
+        <>       
         <div>
           <div className="title">        
           <h1><Typography use="headline5">Cadastrar Ítem</Typography></h1>   
           </div>
-        <section className="containerAbas">
-        <input type="radio" name="abas" class="abas" id="aba1" checked/>
-        <label for="aba1">Dados do produto</label>
-        <div>
-        <form  onSubmit={handleSubimit}>      
-            <div className="formContainer"> 
-            
+          <div className="abas">
+          <Grid>
+              <GridRow>
+                  <GridCell span={12}>
+                          <span onClick={()=>showAproduct()}>Produto</span> 
+                          <span onClick={()=>showAcomplement()} >Complemento</span>
+                          <span onClick={()=>showAimage()} >Imagens</span>
+                  </GridCell>
+              </GridRow>
+          </Grid>
+          </div>
+        {   aproduct && !product &&
+            <form  onSubmit={handleSubimit}>      
+            <div className="formContainer">            
                 <Grid>
+                    <GridRow>
+                        <GridCell>
+                        <h2><Typography use="headline5">Dados do Produto</Typography></h2>
+                        </GridCell>
+                    </GridRow>
                     <GridRow>
                         <GridCell span={12}>
                             <GridRow>
-                                <GridCell span={6}>
-                                    <TextField type="text" name="name" fullwidth placeholder="Nome"  />
-                                </GridCell>
-                                <GridCell span={6}>
-                                    <TextField type="text" name="description" fullwidth placeholder="Descrição"  />
-                                </GridCell>
-                            </GridRow>
-                            <GridRow>
-                                <GridCell span={4}>
-                                    <TextField type="text" name="code" fullwidth placeholder="Código"  />
-                                </GridCell>
-                                <GridCell span={4}>
-                                    <TextField type="text" name="value" fullwidth placeholder="Preço"  />
-                                </GridCell>
-                                <GridCell span={4}>
-                                
-                                    <Select label="Selecione a categoria" 
+                            <GridCell span={7}>                                
+                                    <Select label="Selecione a categoria (Obrigatório)" 
                                     options={categories}
                                     onChange={(evt)=>setCategory(evt.currentTarget.value)}
+                                    required
                                     />
                                 </GridCell>
                             </GridRow>
                             <GridRow>
                                 <GridCell span={6}>
-                                    <Select label="Selecione a impressora"   
+                                    <TextField type="text" name="name" fullwidth placeholder="Nome (Obrigatório)"  required/>
+                                </GridCell>
+                                <GridCell span={6}>
+                                    <TextField type="text" name="description" fullwidth placeholder="Descrição (Obrigatório)" required />
+                                </GridCell>
+                            </GridRow>
+                            <GridRow>
+                                <GridCell span={6}>
+                                    <TextField type="text" name="code" fullwidth placeholder="Código (Opcional)"  />
+                                </GridCell>
+                                <GridCell span={6}>
+                                    <TextField type="text" name="value" fullwidth placeholder="Preço (Obrigatório)"  required/>
+                                </GridCell>
+                            </GridRow>
+                            <GridRow>
+                                <GridCell span={6}>
+                                    <Select label="Selecione a impressora (Obrigatório)"   
                                     options={printers}
                                     onChange={(evt)=>setPrinter(evt.currentTarget.value)}
+                                    required
                                     />
                                 </GridCell>
                             </GridRow>
                             <br/>
                             <GridRow>
-                            <GridCell span={9}>
-                                
+                            <GridCell span={9}>                                
                             </GridCell>               
                             <GridCell span={3}>
-                                <Button label="Cadastrar" outlined icon="add" className={"BtnDefaultSearch"} type="submit"/>
+                                <Button label={loadind?"Aguarde...":"Proximo"} outlined icon="add" className={"BtnDefaultSearch"} type="submit" />
                                 </GridCell>
                             </GridRow>
                         </GridCell>    
                     </GridRow>
                 </Grid>
             </div>
-        </form>
-        </div>
-        <input type="radio" name="abas" class="abas" id="aba2" />
-        <label for="aba2">Complementos</label>
-        <div>Complementos</div>
-
-        </section>
+        </form>}
+        {product && <div>O Produto {product.name} está cadastrado, adicione imagem e Complementos</div>}
+        {acomplement && product &&<div>Complemento do {product.name}</div>}
+        {acomplement && !product &&<div>Nenhum produto cadastrado</div>}
+        {aimage && <div>Imagens</div>}
         </div>
         </>
     )
